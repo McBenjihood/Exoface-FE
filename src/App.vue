676840791ee4237e191c53fdb13cdb573a87b158
b <brt.ozgr@hotmail.com>
2026-09-09 12:12:45 +0200

    favicon und Login background upload

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import LoginView from "./components/LoginView.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <LoginView />
+  <router-view />
 </template>
 
 <style scoped></style>
