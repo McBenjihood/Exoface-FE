fad7293e1436840da2eaecd30951ca6d7aa8287b
b <brt.ozgr@hotmail.com>
2026-09-09 12:17:21 +0200

    Login + ResetPassword pages Version 001 - closed #5

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "./components/LoginView.vue";
-import StudentView from "./components/StudentView.vue";
-import TeacherView from "./components/TeacherView.vue";
+import ResetPasswordView from "./components/ResetPasswordView.vue";
 
 const routes = [
   { path: "/", redirect: "/login" },
   { path: "/login", component: LoginView },
+  { path: "/resetpassword", component: ResetPasswordView },
 ];
 
 const router = createRouter({
