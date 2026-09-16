3f8b1721f93b51a9ba9f1fc6d02820fb53a0c2b6
b <brt.ozgr@hotmail.com>
2026-09-16 11:09:17 +0200

    Layout & Profile Update - closed #4

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import Navbar from "./components/Navbar.vue";
+
+const route = useRoute();
+</script>
 
 <template>
+  <Navbar v-if="!route.meta.open" />
   <router-view />
 </template>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const open = ref(false);
+const email = ref("lehrer@exoface.ch");
+</script>
 <template>
   <nav class="navbar">
     <router-link class="logo" to="/">
       <img src="/logo.png" alt="logo" />
     </router-link>
 
-    <button class="avatar"><img src="/avatar.svg" /></button>
+    <div class="dropdown">
+      <button class="avatar" @click="open = !open">
+        <img src="/avatar.svg" alt="avatar" />
+      </button>
+
+      <div v-if="open" class="menu">
+        <div class="email">{{ email }}</div>
+
+        <router-link to="/login" class="logout">Abmelden</router-link>
+      </div>
+    </div>
   </nav>
 </template>
 
@@ -32,6 +47,11 @@
   display: block;
 }
 
+.dropdown {
+  margin-left: auto;
+  position: relative;
+}
+
 .avatar {
   margin-left: auto;
   height: 35px;
@@ -48,5 +68,37 @@
   height: 100%;
   width: 100%;
   display: block;
+}
+
+.menu {
+  position: absolute;
+  right: 0;
+  top: 45px;
+  min-width: 160px;
+  background: white;
+  border: 1px solid #dde5ea;
+  border-radius: 8px;
+  padding: 6px;
+}
+.email {
+  padding: 8px 10px 10px;
+  font-size: 12px;
+  color: #5a6b77;
+  border-bottom: 1px solid #dde5ea;
+  margin-bottom: 4px;
+}
+.menu a {
+  display: block;
+  padding: 8px 10px;
+  color: #333;
+  text-decoration: none;
+}
+
+.menu a.logout {
+  color: red;
+}
+
+.menu a:hover {
+  background: #5a6b77;
 }
 </style>
